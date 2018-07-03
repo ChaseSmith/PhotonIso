08af0f969f01252c44b961533081800a3012c9ef
Chase Smith <chsm5267@colorado.edu>
2018-07-03 14:09:52 -0600

    closed function

--- TreeMaker.h
+++ TreeMaker.h
@@ -1,49 +1,25 @@
-// --- need to check all these includes...
-#include <fun4all/SubsysReco.h>
-#include <vector>
-#include <calobase/RawTowerGeom.h>
-#include <calobase/RawTower.h>
-#include <calobase/RawClusterContainer.h>
-#include <calobase/RawCluster.h>
-#include <calobase/RawClusterUtility.h>
-#include "TMath.h"
 class PHCompositeNode;
 
-class ClusterIso: public SubsysReco
+class TreeMaker: public SubsysReco
 {
 
  public:
 
-  ClusterIso(const std::string& ,float pTCut, float coneSize);
+  TreeMaker(const std::string &name="TreeMaker.root", int embed_id = 0);
 
   int Init(PHCompositeNode*);
   int process_event(PHCompositeNode*);
   int End(PHCompositeNode*);
-  void setpTCut(float x);
-  void setConeSize(float x);
-  float getpTCut();
-  float getConeSize();
 
  private:
-  float vx;
-  float vy;
-  float vz;
 
-  float deltaR( float eta1, float eta2, float phi1, float phi2 ) {
+  TFile *_f;
 
-    float deta = eta1 - eta2;
-    float dphi = phi1 - phi2;
-    if ( dphi > 3.14159 ) dphi -= 2 * 3.14159;
-    if ( dphi < -3.14159 ) dphi += 2 * 3.14159;
+  TTree *_tree;
 
-    return TMath::Power(( deta*deta + dphi*dphi),.5);
+  std::string _foutname;
 
-  }
-  bool towerInCluster(RawCluster* cluster, RawTower* tower){
-    return cluster->get_towermap().cend() != cluster->get_towermap().find(tower->get_key());
-  }
-
-  float pTCut;
-  float coneSize;
+  int _embed_id;
+  double _b_et_iso;
 
 };
