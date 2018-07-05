610604a4a1528b5ca33be5a3de3b6e33949ec7ac
FrancescoVassalli <Francesco.Vassalli@colorado.edu>
2018-07-05 18:17:50 -0400

    debug

--- ClusterIso.C
+++ ClusterIso.C
@@ -112,6 +112,7 @@ int ClusterIso::process_event(PHCompositeNode *topNode)
       float cluster_eta = E_vec_cluster.pseudoRapidity(); //may need to chagne the eta after it is set.  Needs to be in same reference frame as the towers 
       float cluster_phi = E_vec_cluster.phi();
       float pt = cluster_energy / cosh( cluster_eta );
+      std::cout<<pt<<'\n';
       float isoEt=NAN;
       //if (pt < pTCut) continue; 
       //for each cluster go through all of the towers that are not in that cluster 
