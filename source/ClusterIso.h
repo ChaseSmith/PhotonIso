a1c1c6942e357d42de03046dc4c8035b5304e57b
Chase Smith <chsm5267@colorado.edu>
2018-07-11 17:01:12 -0600

    Moved correct source files into PhotonIso

--- source/ClusterIso.h
+++ source/ClusterIso.h
@@ -2,78 +2,62 @@
 #ifndef ClusterIso_h 
 #define ClusterIso_h
 
-// --- need to check all these includes...
-
-//#include <fun4all/Fun4AllServer.h>  //can't find
-#include <phool/getClass.h>         //good
-#include <phool/PHCompositeNode.h>  //good
-
-//#include <calotrigger/CaloTriggerInfo.h> //can't find, this is for triggers
-#include <calobase/RawClusterContainer.h> //good
-#include <calobase/RawCluster.h>          //good
-#include <calobase/RawClusterUtility.h>   //good
-
-#include <calobase/RawTowerContainer.h> //good
-//#include <calobase/RawTowerGeomContainer_Cylinderv1.h> //can't find
-#include <calobase/RawTowerGeomContainer.h>  //good
-
-//#include <g4main/PHG4Particle.h> //can't find
-//#include <g4main/PHG4VtxPoint.h> //can't find
-#include <g4vertex/GlobalVertex.h>    //good
-#include <g4vertex/GlobalVertexMap.h> //good
-
+#include <fun4all/SubsysReco.h>
+#include <CLHEP/Vector/ThreeVector.h>
 #include <cmath>
-class PHCompositeNode;
 
+class PHCompositeNode;
+class Hep3Vector;
+class RawTowerGeom;
+
+/** \Brief Tool to find isolation energy of each EMCal cluster.
+ * 
+ * This tool finds isoET of clusters by summing towers energy
+ * in a cone of radius R around the cluster and subtracting 
+ * the cluster from the sum
+ */
 
 class ClusterIso: public SubsysReco
 {
-
 public:
-
+  /**
+   * Constructor for ClusterIso Class
+   */
   ClusterIso(const std::string& ,float eTCut, float coneSize);
 
   virtual int Init(PHCompositeNode*);
   virtual int process_event(PHCompositeNode*);
   virtual int End(PHCompositeNode*);
+
   void seteTCut(float x);
   void setConeSize(float x);
   const float geteTCut();
-  const CLHEP::Hep3Vector getVertex();
   const float getConeSize();
-
+  const CLHEP::Hep3Vector getVertex();
 
 private:
-  float m_eTCut;
-  float m_coneSize;
-  float m_vx;
-  float m_vy;
-  float m_vz;
+  double getTowerEta(RawTowerGeom* tower_geom, double vx, double vy, double vz); 
+  float m_eTCut; ///< cluster must be over this energy set in constructor
+  float m_coneSize; ///< delta R around cluster that is considered
+  float m_vx; ///< new vertex x value  
+  float m_vy; ///< new vertex y value 
+  float m_vz; ///< new vertex z value
 };
 
-inline double getTowerEta(RawTowerGeom* tower_geom, double vx, double vy, double vz) 
-  {
-    if(vx==0&&vy==0&&vz==0){
-      return tower_geom->get_eta();
-    }
-    else{
-     double r= tower_geom->get_center_radius();
-     double x = r*cos(tower_geom->get_phi())-vx;
-     double y = r*sin(tower_geom->get_phi())-vy;
-     double z = r/tan(2*atan2(exp(-1*tower_geom->get_eta()),1))-vz;
-     r= sqrt(x*x+y*y);
-     return -log(tan(atan2(r,z)/2.));
-    }
-}
 
+
+/** \Brief Function to find delta R between 2 objects
+ *
+ * Takes eta and phi of each object and returns the difference 
+ * of the etas and phis added in quadrature. Used to find towers
+ * inside a cone of delta R around a cluster.
+ */
 inline const float deltaR( float eta1, float eta2, float phi1, float phi2 ) {
-
-    float deta = eta1 - eta2;
-    float dphi = phi1 - phi2;
-    if ( dphi > 3.14159 ) dphi -= 2 * 3.14159;
-    if ( dphi < -3.14159 ) dphi += 2 * 3.14159;
-
-    return sqrt( deta*deta + dphi*dphi);
-
+  float deta = eta1 - eta2;
+  float dphi = phi1 - phi2;
+  if ( dphi > M_PI ) dphi -= 2 * M_PI ; //corrects to keep range -pi to pi
+  if ( dphi < -1*M_PI ) dphi += 2 * M_PI ; //corrects to keep range -pi to pi
+  return sqrt( deta*deta + dphi*dphi);
 }
-#endif //ClusterIso_h
+
+#endif
