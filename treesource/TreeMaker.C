5fc33ccd030a9380610ad7f7234ff6bdfb1f8ea8
Chase Smith <chsm5267@colorado.edu>
2018-08-13 12:06:55 -0600

    Could be referencing map wrong

--- treesource/TreeMaker.C
+++ treesource/TreeMaker.C
@@ -229,9 +229,8 @@ int TreeMaker::process_event(PHCompositeNode *topNode)
     RawCluster::TowerConstRange begin_end = cluster->get_towers();
     for (RawCluster::TowerConstIterator rtiter = begin_end.first; rtiter != begin_end.second; ++rtiter) 
     {
-      //RawTower *tower = rtiter->second;
-      //RawTowerGeom *tower_geom = geomEM->get_tower_geometry(tower->get_key());
-      RawTowerGeom *tower_geom = geomEM->get_tower_geometry(rtiter->second);
+      RawTower *tower = rtiter->first;
+      RawTowerGeom *tower_geom = geomEM->get_tower_geometry(tower->get_key());
       std::cout<<"Tower Key: "<<tower->get_key()<<std::endl;
 
       tower_phi[counter] = tower_geom->get_phi();
