14c5898dabe7499e048643b8464d8e6b05d8551a
Chase Smith <chsm5267@colorado.edu>
2018-08-13 17:10:57 -0600

    testing tower theory

--- treesource/TreeMaker.C
+++ treesource/TreeMaker.C
@@ -301,7 +301,7 @@ int TreeMaker::process_event(PHCompositeNode *topNode)
       if(dif_phi>TMath::Pi()){dif_phi -= 2*TMath::Pi();} //make sure dif_phi is between -pi and pi
       else if(dif_phi<-1*TMath::Pi()){dif_phi += 2*TMath::Pi();}
 
-      if(fabs(dif_eta) <= 0.08 and fabs(dif_phi) <= 0.08)
+      if(fabs(dif_eta) < 0.08 and fabs(dif_phi) < 0.08)
       {
         std::cout<<"dif eta: "<<dif_eta<<std::endl;
         std::cout<<"dif phi: "<<dif_phi<<std::endl;
