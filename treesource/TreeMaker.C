70d8865eb4a5c4ca0b0df14aff8d5beae038119f
Chase Smith <chsm5267@colorado.edu>
2018-08-14 11:19:45 -0600

    Found problem in RawTowerBuilder

--- treesource/TreeMaker.C
+++ treesource/TreeMaker.C
@@ -203,14 +203,6 @@ int TreeMaker::process_event(PHCompositeNode *topNode)
     
   }
 
-  RawTowerContainer *Emtow = findNode::getClass<RawTowerContainer>(topNode, "TOWER_CALIB_CEMC");
-  std::cout<<"Size of RawTowerContainer for EMCal: "<<Emtow->size()<<std::endl;
-  
-  
-
-  
-
-  /*
   //////////////////////////////////////Find cluster information/////////////////////////////////////////////////////
   _b_cluster_n = 0;
   _b_tower_n = 0;
@@ -334,7 +326,6 @@ int TreeMaker::process_event(PHCompositeNode *topNode)
 
     _b_cluster_n++;
   }
-  */
 
   _tree->Fill();
 
