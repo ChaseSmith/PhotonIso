0cf0bbb9234fc53df1c762bdeea9362d436899ff
Chase Smith <chsm5267@colorado.edu>
2018-08-15 11:20:33 -0600

    Suspicious of max tower algorithm

--- treesource/TreeMaker.C
+++ treesource/TreeMaker.C
@@ -45,12 +45,12 @@
 ChaseTower findMaxTower(std::vector<ChaseTower> towers)
 {
   ChaseTower MaxTower;
-  MaxTower.setKey(towers.at(0).getKey());
-  MaxTower.setEnergy(towers.at(0).getEnergy());
-  MaxTower.setPhi(towers.at(0).getPhi());
-  MaxTower.setEta(towers.at(0).getEta());
+  MaxTower.setKey(0);
+  MaxTower.setEnergy(0);
+  MaxTower.setPhi(0);
+  MaxTower.setEta(0);
 
-  for(unsigned int i = 1; i < towers.size(); i++)
+  for(unsigned int i = 0; i < towers.size(); i++)
   {
     if(towers.at(i).getEnergy() > MaxTower.getEnergy())
     {
