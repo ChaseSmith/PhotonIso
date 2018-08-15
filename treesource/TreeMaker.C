0b7ac68e2f68b275e03897771d77325b2050c40c
Chase Smith <chsm5267@colorado.edu>
2018-08-15 11:30:01 -0600

    make sure we don't double found max tower

--- treesource/TreeMaker.C
+++ treesource/TreeMaker.C
@@ -287,7 +287,6 @@ int TreeMaker::process_event(PHCompositeNode *topNode)
       double this_phi = tower_geom->get_phi();
       double this_eta = tower_geom->get_eta();
       double this_energy = tower->get_energy();
-      double this_keytype = tower->get_key();
       double dif_eta = this_eta - MaxTower.getEta();
       double dif_phi = this_phi - MaxTower.getPhi();
       _b_tower_phi[_b_tower_n] = tower_geom->get_phi();
@@ -298,11 +297,11 @@ int TreeMaker::process_event(PHCompositeNode *topNode)
       if(dif_phi > TMath::Pi()){dif_phi -= 2*TMath::Pi();} //make sure dif_phi is between -pi and pi
       else if(dif_phi < -1*TMath::Pi()){dif_phi += 2*TMath::Pi();}
 
-      if(fabs(dif_eta) < 0.08 and fabs(dif_phi) < 0.08 and this_keytype != MaxTower.getKey())
+      if(fabs(dif_eta) < 0.08 and fabs(dif_phi) < 0.08 )
       {
-        std::cout<<"ANOTHER TOWER PASSED THE CUT "<<std::endl;
-        std::cout<<"tower eta: "<<this_eta<<std::endl;
-        std::cout<<"tower phi: "<<this_phi<<std::endl;
+        //std::cout<<"ANOTHER TOWER PASSED THE CUT "<<std::endl;
+        //std::cout<<"tower eta: "<<this_eta<<std::endl;
+        //std::cout<<"tower phi: "<<this_phi<<std::endl;
         ChaseTower temp;
         temp.setEta(this_phi);
         temp.setPhi(this_eta);
